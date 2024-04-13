140a61ec183843420b0a109fb833940c8ea11021
sarkar niloy <sarkarniloy102@gmail.com>
2024-04-13 16:22:47 +0600

    custom hook for category

--- src/Home/PopularMenu/PopularMenu.jsx
+++ src/Home/PopularMenu/PopularMenu.jsx
@@ -1,28 +1,32 @@
-import { useEffect, useState } from "react";
+
 import SectionTitle from "../../Shared/SectionTitle/SectionTitle";
 import MenuItems from "../../Shared/MenuItems/MenuItems";
 import { NavLink } from "react-router-dom";
+import useMenu from "../../Hooks/useMenu";
 
 
 const PopularMenu = () => {
-    const [menu, setmenu] = useState([]);
-    useEffect(() => {
-        fetch('popular.json')
-            .then(res => res.json())
-            .then(data => {
-                const popularItems = data.filter(item => item.category === 'Popular');
-                setmenu(popularItems)
-            })
-    }, [])
+    const [category] = useMenu();
+    const popular = category.filter(item =>item.category === 'Chocolate List')
+    // const [menu, setmenu] = useState([]);
+    // useEffect(() => {
+    //     fetch('popular.json')
+    //         .then(res => res.json())
+    //         .then(data => {
+    //             const popularItems = data.filter(item => item.category === 'Popular');
+    //             setmenu(popularItems)
+    //         })
+    // }, [])
     return (
         <section className="mb-12">
             <SectionTitle
                 heading={"From Our Categories"}
                 subheading={"Popular Items"}>
             </SectionTitle>
+           
             <div className="grid md:grid-cols-2 gap-4">
                 {
-                    menu.map(item => <MenuItems
+                    popular.map(item => <MenuItems
                         key={item._id}
                         item={item}
                     ></MenuItems>)
--- src/Hooks/useMenu.jsx
+++ src/Hooks/useMenu.jsx
@@ -1,10 +1,17 @@
+import { useEffect, useState } from "react";
 
 const useMenu = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-};
+    const [category, setcategory] = useState([]);
+    const [loading, setloading] = useState(true);
+    useEffect(() => {
+        fetch('menu.json')
+            .then(res => res.json())
+            .then(data => {
+                setcategory(data);
+                setloading(false);
+            })
+    }, [])
+    return [category, loading];
+}
 
 export default useMenu;
