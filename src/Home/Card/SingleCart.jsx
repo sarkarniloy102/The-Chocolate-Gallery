925952c2ab4bcb66c9df3a00d8984ea971d387e7
sarkar niloy <sarkarniloy102@gmail.com>
2024-04-17 03:09:45 +0600

    some changes in cart

--- src/Home/Card/SingleCart.jsx
+++ src/Home/Card/SingleCart.jsx
@@ -5,11 +5,18 @@ const SingleCart = ({ item }) => {
     return (
         <section>
             <div className="card card-compact w-96 h-96  shadow-xl">
-                <figure><img src={image} className="" alt="Chocolate" /></figure>
+
+                <figure className="relative">
+                    <img src={image} className="" alt="Chocolate" />
+                    <p className="absolute top-2 right-2 text-white text-xs  rounded-lg px-4  py-2 bg-black ">{price}</p>
+                </figure>
+
+
                 <div className="card-body text-black">
                     <h2 className="card-title">{name}</h2>
+
                     <p>{details}</p>
-                    <p>Price: {price}</p>
+
                     <div className="card-actions justify-center">
                         <button className="btn bg-slate-200 text-yellow-500">Add to cart</button>
                     </div>
