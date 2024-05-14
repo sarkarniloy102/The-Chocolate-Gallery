2f84febc2ed2a206864884249e60d3464569457c
sarkar niloy <sarkarniloy102@gmail.com>
2024-05-14 23:29:58 +0600

    find cart by user email

--- src/Hooks/useCart.jsx
+++ src/Hooks/useCart.jsx
@@ -1,13 +1,16 @@
 import { useQuery } from "@tanstack/react-query";
 import useAxiosSecure from "./useAxiosSecure";
+import { useContext } from "react";
+import { AuthContext } from "../Providers/AuthProvider";
 
 
 const useCart = () => {
-    const axiosSecure = useAxiosSecure()
+    const axiosSecure = useAxiosSecure();
+    const { user } = useContext(AuthContext);
     const { data: cart = [] } = useQuery({
-        queryKey: ['cart'],
+        queryKey: ['cart', user?.email],
         queryFn: async () => {
-            const res = await axiosSecure.get('/carts',)
+            const res = await axiosSecure.get(`/carts?emai=${user.email}`,)
             return res.data;
         },
 
