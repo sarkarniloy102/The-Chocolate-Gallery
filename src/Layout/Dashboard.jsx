a397fd86fbbfe9023ded3e34594d5b2a741a7816
sarkar niloy <sarkarniloy102@gmail.com>
2024-06-05 04:20:41 +0600

    table structure for user order cart

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -1,5 +1,5 @@
 import { FaCalendar, FaHome, FaList, FaPaypal, FaShoppingCart } from "react-icons/fa";
-import { FaStar } from "react-icons/fa6";
+import { FaMessage, FaStar } from "react-icons/fa6";
 import { NavLink, Outlet } from "react-router-dom";
 
 
@@ -7,7 +7,7 @@ const Dashboard = () => {
     return (
         <div className="flex">
             {/* side bar */}
-            <div className="w-64 min-h-full bg-sky-300">
+            <div className="w-64 min-h-screen bg-sky-300">
                 <ul className="menu text-white">
                     <li>
                         <NavLink to={"/dashboard/userHome"}>
@@ -23,7 +23,7 @@ const Dashboard = () => {
                     </li>
                     <li>
                         <NavLink to={"/dashboard/payment"}>
-                           <FaPaypal></FaPaypal>
+                            <FaPaypal></FaPaypal>
                             Payment History
                         </NavLink>
                     </li>
@@ -35,14 +35,30 @@ const Dashboard = () => {
                     </li>
                     <li>
                         <NavLink to={"/dashboard/addReview"}>
-                           <FaStar></FaStar>
+                            <FaStar></FaStar>
                             Add Review
                         </NavLink>
                     </li>
                     <li>
                         <NavLink to={"/dashboard/bookings"}>
-                           <FaList></FaList>
+                            <FaList></FaList>
                             My Bookings
+                        </NavLink>
+                    </li>
+
+                    {/* divider */}
+                    <div className="divider divider-neutral"></div>
+                    {/*  */}
+                    <li>
+                        <NavLink to={"/"}>
+                            <FaHome ></FaHome>
+                            Home
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to={"/contactus"}>
+                            <FaMessage></FaMessage>
+                            Contact
                         </NavLink>
                     </li>
                 </ul>
--- src/Pages/Dashboard/ShowCart/ShowCart.jsx
+++ src/Pages/Dashboard/ShowCart/ShowCart.jsx
@@ -1,9 +1,82 @@
+import useCart from "../../../Hooks/useCart";
 
 
 const ShowCart = () => {
+    const [cart] = useCart();
+
+    const totalPrice = cart.reduce((sum, item) => {
+        const price = parseInt(item.price.replace("tk", ""), 10);
+        return sum + price;
+    }, 0);
     return (
         <div>
-            <h2>All cart</h2>
+            <h2 className="text-4xl text-center text-yellow-600 my-10"> My Order</h2>
+
+            <div className=" mx-auto py-6  rounded-xl shadow-lg border w-[75%]">
+                <div className="flex justify-evenly items-center">
+                    <h2 className="text-3xl text-black">Number of Orders: {cart.length}</h2>
+                    <h2 className="text-3xl text-black">Total Price: {totalPrice}</h2>
+                    <button className="btn btn-secondary">Pay</button>
+                </div>
+                {/* table */}
+                <div className="overflow-x-auto m-5 rounded-md shadow-lg border">
+                    <table className="table ">
+                        {/* head */}
+                        <thead className="bg-yellow-500 text-white text-normal">
+                            <tr>
+                                <th>
+                                    #
+                                </th>
+                                <th>Image</th>
+                                <th>Name</th>
+                                <th>Price</th>
+                                <th>Auction</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {/* map cart */}
+                            {
+                                cart.map(item =>
+                                    <tr key={item._id}>
+                                        <th>
+                                            <label>
+                                                <input type="checkbox" className="checkbox" />
+                                            </label>
+                                        </th>
+                                        <td>
+                                            <div className="flex items-center gap-3">
+                                                <div className="avatar">
+                                                    <div className="mask mask-squircle w-12 h-12">
+                                                        <img src="https://img.daisyui.com/tailwind-css-component-profile-2@56w.png" alt="Avatar Tailwind CSS Component" />
+                                                    </div>
+                                                </div>
+                                                <div>
+                                                    <div className="font-bold">Hart Hagerty</div>
+                                                    <div className="text-sm opacity-50">United States</div>
+                                                </div>
+                                            </div>
+                                        </td>
+                                        <td>
+                                            Zemlak, Daniel and Leannon
+                                            <br />
+                                            <span className="badge badge-ghost badge-sm">Desktop Support Technician</span>
+                                        </td>
+                                        <td>Purple</td>
+                                        <th>
+                                            <button className="btn btn-ghost btn-xs">details</button>
+                                        </th>
+                                    </tr>
+                                )
+                            }
+
+
+                        </tbody>
+
+
+                    </table>
+                </div>
+            </div>
+
         </div>
     );
 };
