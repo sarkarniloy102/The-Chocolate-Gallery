c7413602fa78eaeafdf815711fca35858f7024cb
sarkar niloy <sarkarniloy102@gmail.com>
2024-06-23 15:29:45 +0600

    delete user & set role(admin)

--- src/Pages/Dashboard/Admin/Allusers/Allusers.jsx
+++ src/Pages/Dashboard/Admin/Allusers/Allusers.jsx
@@ -2,17 +2,78 @@ import { useQuery } from "@tanstack/react-query";
 import SectionTitle from "../../../../Shared/SectionTitle/SectionTitle";
 import useAxiosSecure from "../../../../Hooks/useAxiosSecure";
 import { FaTrash, FaUserSecret } from "react-icons/fa";
+import Swal from "sweetalert2";
 
 const Allusers = () => {
     const axiosSecure = useAxiosSecure();
 
-    const { data: users = [] } = useQuery({
+    const { data: users = [], refetch } = useQuery({
         queryKey: ["users"],
         queryFn: async () => {
             const res = await axiosSecure.get('/users');
             return res.data;
         }
     })
+
+    // delete user
+    const handleDelete = user => {
+
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                axiosSecure.delete(`/users/${user._id}`)
+                    .then(res => {
+                        console.log(res);
+                        if (res.data.deletedCount > 0) {
+                            refetch();
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "Your file has been deleted.",
+                                icon: "success"
+                            });
+                        }
+                    })
+            }
+
+        });
+    }
+
+    // update role admin/moderator
+    const handleRole = user => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, Make Admin"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                axiosSecure.patch(`/users/admin/${user._id}`)
+                    .then(res => {
+                        console.log(res);
+                        if (res.data.modifiedCount > 0) {
+                            refetch();
+                            Swal.fire({
+                                title: "Role Changed",
+                                text: `${user.name} is admin now`,
+                                icon: "success"
+                            });
+                        }
+                    })
+            }
+
+        });
+    }
+
     return (
         <div>
             <SectionTitle heading={"MANAGE USERS"} subheading={"How Many?"} ></SectionTitle>
@@ -26,8 +87,8 @@ const Allusers = () => {
                     <table className="table ">
                         {/* head */}
                         <thead className="bg-yellow-500 text-white text-normal">
-                            <tr>
-                                <th>Serial No</th>
+                            <tr className="text-lg">
+                                <th></th>
                                 <th>Name</th>
                                 <th>Email</th>
                                 <th>Role</th>
@@ -38,7 +99,7 @@ const Allusers = () => {
                             {/* map cart */}
                             {
                                 users.map((user, idx) =>
-                                    <tr  key={user._id}>
+                                    <tr key={user._id}>
                                         <th> {idx + 1} </th>
 
                                         <td>
@@ -47,11 +108,13 @@ const Allusers = () => {
                                         <td>{user.email}</td>
 
                                         <th>
-                                            <button className="btn btn-ghost  " ><FaUserSecret className="text-xl" /></button>
+                                            {user.role === 'admin' ? 'Admin' :
+                                                <button onClick={() => handleRole(user)} className="btn btn-ghost  " ><FaUserSecret className="text-xl" /></button>
+                                            }
                                         </th>
 
                                         <th>
-                                            <button className="btn btn-ghost  text-red-600" ><FaTrash className="text-xl"></FaTrash></button>
+                                            <button onClick={() => handleDelete(user)} className="btn btn-ghost  text-red-600" ><FaTrash className="text-xl"></FaTrash></button>
                                         </th>
                                     </tr>
                                 )
